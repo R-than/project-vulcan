8b5739b7971d15e5ba62a86ac35ee2ce3dd9e4aa
Lexie Silva <lexie.m.silva@gmail.com>
2023-12-14 02:54:44 -0800

    added header and logo, still working on hamburger menu, added slideshow images into the html, styled in scss and then scripted in js to flip through each image, white wave styled and positioned to bottom of hero image, text and buttons fine tuned.

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,13 @@
-// Write your JavaScript here...
+const slides = document.querySelectorAll('.slide');
+var currentSlide = 0;
+
+// Show the first slide immediately
+slides[currentSlide].classList.add('active');
+
+function showSlide() {
+ slides[currentSlide].classList.remove('active');
+ currentSlide = (currentSlide+1)%slides.length;
+ slides[currentSlide].classList.add('active');
+}
+
+setInterval(showSlide, 3000);
